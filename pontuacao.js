0c2fea86dbd06cd2f2c77e3651d204a3dcd24bd7
MUNIZ666__ <lucas.goncalvez.muniz@escola.pr.gov.br>
2026-04-07 11:55:05 +0000

    versao1

---
+++ pontuacao.js
@@ -0,0 +1,12 @@
+//sistema de pontuação de um jogo
+const nomeJogador = "Elisa";
+let faseatual = 3;
+let vidasrestantes = 2;
+
+let pontosPorMissao = [85, 90, 60, 100];
+
+//pontosPorMissao.length
+let pontuacaototal = 0;
+
+console.log(`Seja bem vinda ${nomeJogador}!´  );
+console.log(pontosPorMissao);
