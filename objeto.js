a15e14c5ae0d1574cc360493c9117da599cacfb2
MUNIZ666__ <lucas.goncalvez.muniz@escola.pr.gov.br>
2026-04-27 11:53:00 +0000

    v1

---
+++ objeto.js
@@ -0,0 +1,16 @@
+const bancoautomotivo = {
+    material: "couro natural",
+    cor: "caramelo",
+    tamanho: "padrão",
+    costura: false
+}
+
+//console.log("material escolhido: ", bancoautomotivo.material);
+
+//bancoautomotivo.concluido = true;
+//console.log(bancoautomotivo);
+const blusa = {
+    material: "lã",
+    cor: "preto",
+    tamanho: "G",
+}
